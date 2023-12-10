3d133006383ac8a7d3ac0aec422abe3c587a89e9
unknown <1328635598@qq.com>
2023-12-10 16:41:38 +0800

    update at 2023-12-10 16:41:38

--- scripts/auto_push.sh
+++ scripts/auto_push.sh
@@ -1,10 +1,6 @@
 time=$(date +"%Y-%m-%d %H:%M:%S")
 commit_message="update at $time"
 
-# pull remote
-git pull --rebase
-echo "git pull done"
-
 # 添加文件到暂存区
 git add ..
 echo "git add done"
@@ -12,6 +8,10 @@ echo "git add done"
 # 创建提交
 git commit -m "$commit_message"
 echo "git commit done"
+
+# pull remote
+git pull --rebase
+echo "git pull done"
 
 # 推送到GitHub
 git push origin source
