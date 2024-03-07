24a4736df5cf73fc56ba3b127a5cb2f96b752284
unknown <1328635598@qq.com>
2024-03-08 01:01:23 +0800

    update posts at 2024-03-08 01:01:23.

--- self_scripts/auto_push.sh
+++ self_scripts/auto_push.sh
@@ -8,10 +8,16 @@ echo "git add done"
 
 # 创建提交
 git commit -m "$commit_message"
+if [ $? -ne 0 ]; then
+    exit 0
+fi
 echo "git commit done"
 
 # pull remote
 git pull --rebase
+if [ $? -ne 0 ]; then
+    exit 0
+fi
 echo "git pull done"
 
 # 推送到GitHub
